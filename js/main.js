7c85456c78ab8314cf87b1459eda13937509f8d9
packercj <packerc@gmail.com>
2016-07-26 12:19:43 -0600

    additon of more script and bug fix

--- js/main.js
+++ js/main.js
@@ -1,31 +1,45 @@
 var question;
-var answer = ["Your future looks intersting", "You are in for some moneies"];
+var answers = ["Your future looks intersting", "You are in for some moneies", "Your future doesn't look so good", "You will find that pokemon"];
 var input = document.getElementById("input");
 var log;
+var questionsAsked = [];
 
 function roll(){
+  var valid = validateInput();
+  
+  if (!valid) {
+    return;
+  }
+
+  addToQuestionsAsked();
   saveQuestion();
   returnAnswer();
 }
 
 function saveQuestion() {
   question = input.value;
-  if (input.value === ""){
-    document.createElementByID("input").innerHTML = ("guess again")
-  };
   input.value = "";
 }
+function validateInput() {
+  var isValid = true;
 
+  if (input.value.length <= 0) {
+    document.getElementById("answer").innerHTML = "Please guess again";
+    isValid = false;
+  }
+
+  return isValid;
+}
 function returnAnswer(){
-  var rn = Math.floor(Math.random() * answer.length);
-  document.getElementById("answer").innerHTML = answer[rn];
+  var rn = Math.floor(Math.random() * answers.length);
+  document.getElementById("answer").innerHTML = answers[rn];
+}
+function addToQuestionsAsked (){
+  questionsAsked.push(input.value);
+  document.getElementById("questions_asked").innerHTML = questionsAsked;
 }
 function clearfields() {
   document.getElementById("answer").innerHTML = "";
   input.value = "";
-}
-
-function returnLog(){
-  document.createElementByID("log")
 }
 //clear will reset input and returned answer
